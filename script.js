d050990376658e94cfc2e802dce1f1554a6b30dd
Jake Berg <jacobvictorberg@gmail.com>
2018-03-08 17:14:48 -0500

    almost working

--- script.js
+++ script.js
@@ -1,326 +1,152 @@
 const map = [
-  "  WWWWW ",
-  "WWW   W ",
-  "W   BBOSW ",
-  "WWW BOW ",
-  "WOWWB W ",
-  "W W O WW",
-  "WB XBBOW",
-  "W   O  W",
-  "WWWWWWWW"
+    "  WWWWW ",
+    "WWW   W ",
+    "WOS   W ",
+    "WWW BOW ",
+    "WOWWB W ",
+    "W W O WW",
+    "WB XB OW",
+    "W   O  W",
+    "WWWWWWWW"
 ];
 
-const main = document.querySelector("main");
-var board = []
-
-for (let x = 0; x < map.length; x++) {
-    board[x] = map[x].split("");
-    for (let y = 0; y < map[x].length; y++) {
-        if ([x][y] == "X") {
-            [x][y] = "B";
-        }
-    }
-}
+const board = document.getElementById("board");
+const boxes = document.getElementById("boxes");
+var player = document.getElementById("player");
+var crates = [];
+var playerRow;
+var playerCol;
 
 // Draws the board based on the array above
-function draw(startPos) {
+for (let r = 0; r < map.length; r++) {
+    var row = map[r];
+    var cellClass = [];
+    for (let c = 0; c < row.length; c++) {
+        let cell = row[c];
+        var cellDiv = document.createElement("div");
 
-    for (let i = 0; i < startPos.length; i++) {
-        var newRow = document.createElement("div");
-        newRow.className = ("flexbox");
-        main.appendChild(newRow);
-
-        for (let j = 0; j < startPos[i].length; j++) {
-            let cell = startPos[i][j];
-
-            if (cell === "W") {
-                var newCol = document.createElement("div");
-                newCol.className = ("wall");
-            } else if (cell === " ") {
-                var newCol = document.createElement("div");
-                newCol.className = ("empty");
-            } else if (cell === "O") {
-                var newCol = document.createElement("div");
-                newCol.className = ("empty spot");
-            } else if (cell === "B") {
-                var newCol = document.createElement("div");
-                newCol.className = ("box");
-            } else if (cell === "X") {
-                var newCol = document.createElement("div");
-                newCol.className = ("lightBox");
-            } else if (cell === "S") {
-                var newCol = document.createElement("div");
-                newCol.className = ("player");
-            } else if (cell === "SO") {
-                var newCol = document.createElement("div");
-                newCol.className = ("player");
-            } else if (cell === "BO") {
-                var newCol = document.createElement("div");
-                newCol.className = ("box");
-            }
-            newRow.appendChild(newCol);
-        }
+        if (cell === "B") {
+            cellDiv.className = ("empty");
+            cellClass.push("box");
+        } else if (cell === "W") {
+            cellDiv.className = ("wall");
+            cellClass.push(null);
+        } else if (cell === " ") {
+            cellDiv.className = ("empty");
+            cellClass.push(null);
+        } else if (cell === "O") {
+            cellDiv.className = ("empty spot");
+            cellClass.push(null);
+        } else if (cell === "X") {
+            cellDiv.className = ("empty");
+            cellClass.push("box");
+        } else if (cell === "S") {
+            cellDiv.className = ("empty");
+            cellClass.push(null);
+            player.style.top = r * 40 + "px";
+            player.style.left = c * 40 + "px";
+            playerRow = r;
+            playerCol = c;
+        } 
+        board.appendChild(cellDiv);
     }
+    crates.push(cellClass);
 }
-draw(map);
 
 movePlayer = function (event) {
-    switch (event.key) {
+    let keyStroke = event.key;
+    switch (keyStroke) {
         case "ArrowRight":
-            reset(main);
-            for (i = 0; i < board.length; i++) {
-
-                if (board[i].includes("S")) {
-                    let playerPos = board[i].indexOf("S");
-
-                    if (board[i][playerPos + 1] == " ") {
-                        board[i][playerPos] = " ";
-                        board[i][playerPos + 1] = "S";
-                        var newMap = board;
-                        break;
-                    } else if (board[i][playerPos + 1] == "B" && board[i][playerPos + 2] == "O") {
-                        board[i][playerPos] = " ";
-                        board[i][playerPos + 1] = "S";
-                        board[i][playerPos + 2] = "BO";
-                        var newMap = board;
-                        break;
-
-                        //////////
-                    } else if (board[i][playerPos + 1] == "B" && board[i][playerPos + 1] !== "BO" && board[i][playerPos + 2] !== "W" && board[i][playerPos + 2] !== "B") {
-                        board[i][playerPos] = " ";
-                        board[i][playerPos + 1] = "S";
-                        board[i][playerPos + 2] = "B";
-                        var newMap = board;
-                        break;
-                    } else if (board[i][playerPos + 1] == "O") {
-                        board[i][playerPos] = " ";
-                        board[i][playerPos + 1] = "SO";
-                        var newMap = board;
-                        break;
-                    }
-                }
-
-                if (board[i].includes("SO")) {
-                    let playerOnO = board[i].indexOf("SO");
-                    if (board[i][playerOnO + 1] != "W" && board[i][playerOnO + 1] != "B" ) {
-                        board[i][playerOnO] = "O";
-                        board[i][playerOnO + 1] = "S";
-                        var newMap = board;
-                        break;
-
-                        ////////
-                    } else if (board[i][playerOnO + 1] == "B" && board[i][playerOnO + 2] != "W" && board[i][playerOnO + 2] != "B") {
-                        board[i][playerOnO] = "O";
-                        board[i][playerOnO + 1] = "S";
-                        board[i][playerOnO + 2] = "B";
-                        var newMap = board;
-                        break;
-                    }
-                }
-
-                if (board[i].includes("BO")) {
-                    let BoxOnO = board[i].indexOf("BO");
-                    if (board[i][BoxOnO] == "BO" && board[i][BoxOnO + 1] !== "W") {
-                        board[i][BoxOnO - 1] = " ";
-                        board[i][BoxOnO] = "SO";
-                        board[i][BoxOnO + 1] = "B";
-                        break;
-                    }
-                }
-
+            if (map[playerRow][playerCol + 1] !== "W" && crates[playerRow][playerCol + 1] == null) {
+                playerCol += 1;
+                let moveC = playerCol;
+                player.style.left = moveC * 40 + "px";
+            } else if (crates[playerRow][playerCol + 1] == "box" && map[playerRow][playerCol + 2] !== "W" && crates[playerRow][playerCol + 2] !== "box") {
+                crates[playerRow][playerCol + 1] = null;
+                crates[playerRow][playerCol + 2] = "box";
+                playerCol += 1;
+                let moveC = playerCol;
+                player.style.left = moveC * 40 + "px";
+                var newStuff = crates;
+                move(newStuff)
             }
-            var newMap = board;
-            console.log(newMap)
-            draw(newMap);
             break;
 
         case "ArrowLeft":
-            reset(main);
-            for (i = 0; i < board.length; i++) {
-                if (board[i].includes("S")) {
-                    let playerPos = board[i].indexOf("S");
-
-                    if (board[i][playerPos - 1] == " ") {
-                        board[i][playerPos] = " ";
-                        board[i][playerPos - 1] = "S";
-                        var newMap = board;
-                        break;
-                    } else if (board[i][playerPos - 1] == "B" && board[i][playerPos - 2] == "O") {
-                        board[i][playerPos] = " ";
-                        board[i][playerPos - 1] = "S";
-                        board[i][playerPos - 2] = "BO";
-                        var newMap = board;
-                        break;
-
-                        //////
-                    } else if (board[i][playerPos - 1] == "B" && board[i][playerPos - 1] !== "BO" && board[i][playerPos - 2] !== "W" && board[i][playerPos - 2] !== "B") {
-                        board[i][playerPos] = " ";
-                        board[i][playerPos - 1] = "S";
-                        board[i][playerPos - 2] = "B";
-                        var newMap = board;
-                        break;
-                    } else if (board[i][playerPos - 1] == "O") {
-                        board[i][playerPos] = " ";
-                        board[i][playerPos - 1] = "SO";
-                        var newMap = board;
-                        break;
-                    }
-                }
-                if (board[i].includes("SO")) {
-                    let playerOnO = board[i].indexOf("SO");
-                    if (board[i][playerOnO - 1] != "W" && board[i][playerOnO - 1] != "B") {
-                        board[i][playerOnO] = "O";
-                        board[i][playerOnO - 1] = "S";
-                        var newMap = board;
-                        break;
-
-                        //////
-                    } else if (board[i][playerOnO - 1] == "B" && board[i][playerOnO - 2] != "W" && board[i][playerOnO - 2] != "B") {
-                        board[i][playerOnO] = "O";
-                        board[i][playerOnO - 1] = "S";
-                        board[i][playerOnO - 2] = "B";
-                        var newMap = board;
-                        break;
-                    }
-                }
-                if (board[i].includes("BO")) {
-                    let BoxOnO = board[i].indexOf("BO");
-                    if (board[i][BoxOnO] == "BO" && board[i][BoxOnO - 1] !== "W") {
-                        board[i][BoxOnO + 1] = " ";
-                        board[i][BoxOnO] = "SO";
-                        board[i][BoxOnO - 1] = "B";
-                        break;
-                    }
-                }
+            if (map[playerRow][playerCol - 1] !== "W" && crates[playerRow][playerCol - 1] == null) {
+                playerCol -= 1;
+                let moveC = playerCol;
+                player.style.left = moveC * 40 + "px";
+            } else if (crates[playerRow][playerCol - 1] == "box" && map[playerRow][playerCol - 2] !== "W" && crates[playerRow][playerCol - 2] !== "box") {
+                crates[playerRow][playerCol - 1] = null;
+                crates[playerRow][playerCol - 2] = "box";
+                playerCol -= 1;
+                let moveC = playerCol;
+                player.style.left = moveC * 40 + "px";
+                var newStuff = crates;
+                move(newStuff)
             }
-            var newMap = board;
-            console.log(newMap)
-            draw(newMap);
             break;
 
         case "ArrowUp":
-            reset(main);
-            for (i = 0; i < board.length; i++) {
-
-                if (board[i].includes("S")) {
-                    let playerPos = board[i].indexOf("S");
-                    if (board[i - 1][playerPos] == " ") {
-                        board[i][playerPos] = " ";
-                        board[i - 1][playerPos] = "S";
-                        var newMap = board;
-                        break;
-                    } else if (board[i - 1][playerPos] == "B" && board[i - 2][playerPos] == "O") {
-                        board[i][playerPos] = " ";
-                        board[i - 1][playerPos] = "S";
-                        board[i - 2][playerPos] = "BO";
-                        var newMap = board;
-                        break;
-                    } else if (board[i - 1][playerPos] == "B" && board[i - 2][playerPos] !== "BO" && board[i - 2][playerPos] !== "W") {
-                        board[i][playerPos] = " ";
-                        board[i - 1][playerPos] = "S";
-                        board[i - 2][playerPos] = "B";
-                        var newMap = board;
-                        break;
-                    } else if (board[i - 1][playerPos] == "O") {
-                        board[i][playerPos] = " ";
-                        board[i - 1][playerPos] = "SO";
-                        var newMap = board;
-                        break;
-                    }
-                }
-                if (board[i].includes("SO")) {
-                    let playerOnO = board[i].indexOf("SO");
-                    if (board[i - 1][playerOnO] != "W" && board[i - 1][playerOnO] != "B") {
-                        board[i][playerOnO] = "O";
-                        board[i - 1][playerOnO] = "S";
-                        var newMap = board;
-                        break;
-                    } else if (board[i - 1][playerOnO] = "B" && board[i - 2][playerOnO] != "W") {
-                        board[i][playerOnO] = "O";
-                        board[i - 1][playerOnO] = "S";
-                        board[i - 2][playerOnO] = "B";
-                        var newMap = board;
-                        break;
-                    }
-                }
-                if (board[i].includes("BO")) {
-                    let BoxOnO = board[i].indexOf("BO");
-                    if (board[i][BoxOnO] == "BO" && board[i - 1][BoxOnO] !== "W") {
-                        board[i + 1][BoxOnO] = " ";
-                        board[i][BoxOnO] = "SO";
-                        board[i - 1][BoxOnO] = "B";
-                        break;
-                    }
-                }
+            if (map[playerRow - 1][playerCol] !== "W" && crates[playerRow - 1][playerCol] == null) {
+                playerRow -= 1;
+                let moveR = playerRow;
+                player.style.top = moveR * 40 + "px";
+            } else if (crates[playerRow - 1][playerCol] == "box" && map[playerRow - 2][playerCol] !== "W" && crates[playerRow - 2][playerCol] !== "box") {
+                crates[playerRow - 1][playerCol] = null;
+                crates[playerRow - 2][playerCol] = "box";
+                playerRow -= 1;
+                let moveR = playerRow;
+                player.style.top = moveR * 40 + "px";
+                var newStuff = crates;
+                move(newStuff)
             }
-            var newMap = board;
-            draw(newMap);
             break;
 
         case "ArrowDown":
-            reset(main);
-            for (i = 0; i < board.length; i++) {
-                if (board[i].includes("S")) {
-                    let playerPos = board[i].indexOf("S");
-                    if (board[i + 1][playerPos] == " ") {
-                        board[i][playerPos] = " ";
-                        board[i + 1][playerPos] = "S";
-                        var newMap = board;
-                        break;
-                    } else if (board[i + 1][playerPos] == "B" && board[i + 2][playerPos] == "O") {
-                        board[i][playerPos] = " ";
-                        board[i + 1][playerPos] = "S";
-                        board[i + 2][playerPos] = "BO";
-                        var newMap = board;
-                        break;
-                    } else if (board[i + 1][playerPos] == "B" && board[i + 2][playerPos] !== "BO" && board[i + 2][playerPos] !== "W") {
-                        board[i][playerPos] = " ";
-                        board[i + 1][playerPos] = "S";
-                        board[i + 2][playerPos] = "B";
-                        var newMap = board;
-                        break;
-                    } else if (board[i + 1][playerPos] == "O") {
-                        board[i][playerPos] = " ";
-                        board[i + 1][playerPos] = "SO";
-                        var newMap = board;
-                        break;
-                    }
-                }
-                if (board[i].includes("SO")) {
-                    let playerOnO = board[i].indexOf("SO");
-                    if (board[i + 1][playerOnO] != "W" && board[i + 1][playerOnO] != "B") {
-                        board[i][playerOnO] = "O";
-                        board[i + 1][playerOnO] = "S";
-                        var newMap = board;
-                        break;
-                    } else if (board[i + 1][playerOnO] = "B" && board[i + 2][playerOnO] != "W") {
-                        board[i][playerOnO] = "O";
-                        board[i + 1][playerOnO] = "S";
-                        board[i + 2][playerOnO] = "B";
-                        var newMap = board;
-                        break;
-                    }
-                }
-                if (board[i].includes("BO")) {
-                    let BoxOnO = board[i].indexOf("BO");
-                    if (board[i][BoxOnO] == "BO" && board[i - 1][BoxOnO] !== "W") {
-                        board[i - 1][BoxOnO] = " ";
-                        board[i][BoxOnO] = "SO";
-                        board[i + 1][BoxOnO] = "B";
-                        break;
-                    }
-                }
+            if (map[playerRow + 1][playerCol] !== "W" && crates[playerRow + 1][playerCol] == null) {
+                playerRow += 1;
+                let moveR = playerRow;
+                player.style.top = moveR * 40 + "px";
+            } else if (crates[playerRow + 1][playerCol] == "box" && map[playerRow + 2][playerCol] !== "W" && crates[playerRow + 2][playerCol] !== "box" ) {
+                crates[playerRow + 1][playerCol] = null;
+                crates[playerRow + 2][playerCol] = "box";
+                playerRow += 1;
+                let moveR = playerRow;
+                player.style.top = moveR * 40 + "px";
+                var newStuff = crates;
+                move(newStuff)
             }
-            var newMap = board;
-            draw(newMap);
             break;
+
+        default:
+            console.log(keyStroke)
     }
 }
 
 document.addEventListener("keydown", movePlayer);
 
-function reset(destination) {
-    while (destination.firstChild) {
-        destination.removeChild(destination.firstChild);
+function move(boxDest) {
+    reset();
+    for (let x = 0; x < boxDest.length; x++) {
+        var z = boxDest[x];
+        for (let y = 0; y < z.length; y++) {
+            var foo = z[y];
+            var absoluteCrates = document.createElement("div");
+            if (foo === "box") {
+                absoluteCrates.className = ("box");
+                absoluteCrates.style.top = x * 40 + "px";
+                absoluteCrates.style.left = y * 40 + "px";
+                boxes.appendChild(absoluteCrates);
+            } 
+        }
+    }
+}
+move(crates);
+
+function reset() {
+    while (boxes.firstChild) {
+        boxes.removeChild(boxes.firstChild);
     }
 }
