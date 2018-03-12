9d2e486a375c61dd3499cba74f0bb48b73497568
Jake Berg <jacobvictorberg@gmail.com>
2018-03-12 15:43:09 -0400

    starting to refactor and make code more readable. the win function in now dynamic to whatever map out put in.

--- script.js
+++ script.js
@@ -13,79 +13,64 @@ const map = [
 const board = document.getElementById("board");
 const boxes = document.getElementById("boxes");
 var player = document.getElementById("player");
+var winningSpots = [];
+var cratesSpots = [];
 var crates = [];
 var playerRow;
 var playerCol;
 
 // Draws the board based on the array above
 for (let r = 0; r < map.length; r++) {
-    var row = map[r];
-    var cellClass = [];
+    let row = map[r];
+    var boxPosition = [];
+    var spotPosition = [];
+
     for (let c = 0; c < row.length; c++) {
         let cell = row[c];
         var cellDiv = document.createElement("div");
 
         if (cell === "B") {
             cellDiv.className = ("empty");
-            cellClass.push("box");
         } else if (cell === "W") {
             cellDiv.className = ("wall");
-            cellClass.push(null);
+        } else if (cell === " " || cell === "S") {
+            cellDiv.className = ("empty");
+        } else if (cell === "O" || cell === "X") {
+            cellDiv.className = ("empty spot");
+        }
+        board.appendChild(cellDiv);
+
+        if (cell === "B") {
+            boxPosition.push("box");
+        } else if (cell === "W") {
+            boxPosition.push(null);
         } else if (cell === " ") {
-            cellDiv.className = ("empty");
-            cellClass.push(null);
+            boxPosition.push(null);
         } else if (cell === "O") {
-            cellDiv.className = ("empty spot");
-            cellClass.push(null);
+            boxPosition.push(null);
+            winningSpots.push(String(r) + String(c));
         } else if (cell === "X") {
-            cellDiv.className = ("empty spot");
-            cellClass.push("box");
+            boxPosition.push("box");
+            winningSpots.push(String(r) + String(c));
         } else if (cell === "S") {
-            cellDiv.className = ("empty");
-            cellClass.push(null);
+            boxPosition.push(null);
             player.style.top = r * 40 + "px";
             player.style.left = c * 40 + "px";
             playerRow = r;
             playerCol = c;
         }
-        board.appendChild(cellDiv);
     }
-    crates.push(cellClass);
+    crates.push(boxPosition);
 }
 
-movePlayer = function (event) {
-    let keyStroke = event.key;
-    switch (keyStroke) {
+document.addEventListener("keydown", (event) => {
+    switch (event.key) {
         case "ArrowRight":
-            if (map[playerRow][playerCol + 1] !== "W" && crates[playerRow][playerCol + 1] == null) {
-                playerCol += 1;
-                let moveC = playerCol;
-                player.style.left = moveC * 40 + "px";
-            } else if (crates[playerRow][playerCol + 1] == "box" && map[playerRow][playerCol + 2] !== "W" && crates[playerRow][playerCol + 2] !== "box") {
-                crates[playerRow][playerCol + 1] = null;
-                crates[playerRow][playerCol + 2] = "box";
-                playerCol += 1;
-                let moveC = playerCol;
-                player.style.left = moveC * 40 + "px";
-                var newStuff = crates;
-                move(newStuff)
-            }
+            playerMove(+1, 0, +2, 0)
             break;
 
         case "ArrowLeft":
-            if (map[playerRow][playerCol - 1] !== "W" && crates[playerRow][playerCol - 1] == null) {
-                playerCol -= 1;
-                let moveC = playerCol;
-                player.style.left = moveC * 40 + "px";
-            } else if (crates[playerRow][playerCol - 1] == "box" && map[playerRow][playerCol - 2] !== "W" && crates[playerRow][playerCol - 2] !== "box") {
-                crates[playerRow][playerCol - 1] = null;
-                crates[playerRow][playerCol - 2] = "box";
-                playerCol -= 1;
-                let moveC = playerCol;
-                player.style.left = moveC * 40 + "px";
-                var newStuff = crates;
-                move(newStuff)
-            }
+            playerMove(-1, 0, -2, 0)
             break;
 
         case "ArrowUp":
@@ -97,10 +82,8 @@ movePlayer = function (event) {
                 crates[playerRow - 1][playerCol] = null;
                 crates[playerRow - 2][playerCol] = "box";
                 playerRow -= 1;
-                let moveR = playerRow;
-                player.style.top = moveR * 40 + "px";
-                var newStuff = crates;
-                move(newStuff)
+                player.style.top = playerRow * 40 + "px";
+                moveBox(crates)
             }
             break;
 
@@ -113,49 +96,67 @@ movePlayer = function (event) {
                 crates[playerRow + 1][playerCol] = null;
                 crates[playerRow + 2][playerCol] = "box";
                 playerRow += 1;
-                let moveR = playerRow;
-                player.style.top = moveR * 40 + "px";
-                var newStuff = crates;
-                move(newStuff)
+                player.style.top = playerRow * 40 + "px";
+                moveBox(crates)
             }
             break;
 
         default:
-            console.log(keyStroke)
+            console.log(event.key)
     }
-    checkwin();
+    checkWin();
+});
+
+function playerMove(leftOrRight, upOrDown, boundaryCheckLR, boundaryCheckUD) {
+    const checkForWall = map[playerRow][playerCol + leftOrRight] !== "W";
+    const checkForNull = crates[playerRow][playerCol + leftOrRight] == null;
+    const checkForBox = crates[playerRow][playerCol + leftOrRight] == "box";
+    const checkForExtraBox = crates[playerRow + boundaryCheckUD][playerCol + boundaryCheckLR] !== "box";
+    const checkForWallTwo = map[playerRow + boundaryCheckUD][playerCol + boundaryCheckLR] !== "W"
+    
+    if (checkForWall && checkForNull) {
+        playerCol += leftOrRight;
+        player.style.left = playerCol * 40 + "px";
+
+    } else if (checkForWall && checkForBox && checkForExtraBox && checkForWallTwo) {
+        crates[playerRow][playerCol + leftOrRight] = null;
+        crates[playerRow][playerCol + boundaryCheckLR] = "box";
+        playerCol += leftOrRight;
+        player.style.left = playerCol * 40 + "px";
+        moveBox(crates);
+    }
 }
 
-document.addEventListener("keydown", movePlayer);
-
-function move(boxDest) {
+function moveBox(boxDest) {
     reset();
     for (let x = 0; x < boxDest.length; x++) {
         var z = boxDest[x];
         for (let y = 0; y < z.length; y++) {
-            var foo = z[y];
+            var cellOfCrateSpots = z[y];
             var absoluteCrates = document.createElement("div");
-            if (foo === "box") {
+            if (cellOfCrateSpots === "box") {
                 absoluteCrates.className = ("box");
                 absoluteCrates.style.top = x * 40 + "px";
                 absoluteCrates.style.left = y * 40 + "px";
                 boxes.appendChild(absoluteCrates);
-
-
+                cratesSpots.push(String(x) + String(y));
             }
         }
     }
 }
-move(crates);
+moveBox(crates);
 
 function reset() {
+    cratesSpots = [];
     while (boxes.firstChild) {
         boxes.removeChild(boxes.firstChild);
     }
 }
 
-function checkwin() {
-    if (crates[2][1] == "box" && crates[3][5] == "box" && crates[4][1] == "box" && crates[5][4] == "box" && crates[6][3] == "box" && crates[6][6] == "box" && crates[7][4] == "box"){
-        alert("You win!!!")
+function checkWin() {
+    let cratesSpotsString = cratesSpots.join("");
+    let winningSpotsString = winningSpots.join("");
+    if (cratesSpotsString == winningSpotsString) {
+        document.getElementById("win").style.display = "block";
     }
 }
